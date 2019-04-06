b2f8bb44fa9ebef4daac4adf4aed7305352c422e
Alessandro Fulgini <fuljo97@gmail.com>
2019-04-06 23:40:49 +0200

    Introduce ActionEnum and ActionCardEnum

---
+++ src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/model/action/ActionCardEnum.java
@@ -0,0 +1,29 @@
+package it.polimi.deib.se2019.sanp4.adrenaline.model.action;
+
+/**
+ * Identifies an action card.
+ */
+public enum ActionCardEnum {
+    /** Used when user starts playing */
+    REGULAR("Regular"),
+    /** Regular with first adrenaline action */
+    ADRENALINE1("Regular with adrenaline run"),
+    /** Regular with second adrenaline action */
+    ADRENALINE2("Regular with adrenaline run and shoot"),
+    /** Frenzy mode (x1), used by players from first onwards */
+    FRENZY1("Frenzy - before first player"),
+    /** Frenzy mode (x2), used by players before first */
+    FRENZY2("Frenzy - after first player");
+
+    private final String message;
+
+    ActionCardEnum(String message) {
+        this.message = message;
+    }
+
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
