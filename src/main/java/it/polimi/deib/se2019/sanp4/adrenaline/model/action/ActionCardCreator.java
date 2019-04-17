b7972ec1118dcef8fa71ec6634cac4fca631c245
Alessandro Fulgini <fuljo97@gmail.com>
2019-04-17 17:36:03 +0200

    Remove ActionCardCreator and rename StandardActionCardCreator to match other creators

--- src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/model/action/ActionCardCreator.java
+++ src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/model/action/ActionCardCreator.java
@@ -2,14 +2,40 @@ package it.polimi.deib.se2019.sanp4.adrenaline.model.action;
 
 import it.polimi.deib.se2019.sanp4.adrenaline.common.exceptions.CardNotFoundException;
 
-/* TODO: Add some description */
-public interface ActionCardCreator {
+import java.util.EnumMap;
+import java.util.Map;
+
+/**
+ * Shared object responsible for building action cards from file
+ * and returning them when asked.
+ * The returned action cards are not cloned, but they are immutable
+ * so it's safe (and suggested) to share them among players and matches.
+ */
+public class ActionCardCreator {
+    private final Map<ActionCardEnum, ActionCard> cards;
+
+    /*TODO: Load cards from JSON file*/
+    public ActionCardCreator() {
+        cards = new EnumMap<>(ActionCardEnum.class);
+    }
 
     /**
      * Returns the action card associated to given type.
-     * @param type type of the action card
+     *
+     * @param type type of the action card, not null
      * @return action card associated to type
-     * @throws it.polimi.deib.se2019.sanp4.adrenaline.common.exceptions.CardNotFoundException
+     * @throws CardNotFoundException if requested card type does not exist
      */
-    public ActionCard createActionCard(ActionCardEnum type) throws CardNotFoundException;
+    public ActionCard createActionCard(ActionCardEnum type) throws CardNotFoundException {
+        if(type == null){
+            throw new NullPointerException("Action card type cannot be null");
+        }
+        ActionCard card = cards.get(type);
+
+        if (card == null){
+            throw new CardNotFoundException();
+        }
+
+        return card;
+    }
 }
