6502d0a86a274d85d00d9dbbf227d1b839f8e508
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2019-04-17 17:28:30 +0200

    Add tests for powerup card
    Also fixed implementation of powerup card class.
    Added overloaded method to compute  equality and hash code

---
+++ src/test/java/it/polimi/deib/se2019/sanp4/adrenaline/model/items/powerup/PowerupCardTest.java
@@ -0,0 +1,133 @@
+package it.polimi.deib.se2019.sanp4.adrenaline.model.items.powerup;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.items.ammo.AmmoCube;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+
+public class PowerupCardTest {
+
+    private static String validId = "powerupOne";
+    private static String validName = "Powerup One";
+    private static String validDescription = "Description of Powerup One";
+    private static AmmoCube validColor = AmmoCube.BLUE;
+
+    private static ObjectMapper mapper;
+
+    @BeforeClass
+    public static void setup(){
+        mapper = new ObjectMapper();
+    }
+
+
+    @Test(expected = NullPointerException.class)
+    public void createPowerUpCard_nullParametersProvided_shouldThrowNullPointerException(){
+        new PowerUpCard(null, null, null, null);
+    }
+    @Test(expected = NullPointerException.class)
+    public void createPowerUpCard_nullIdProvided_shouldThrowNullPointerException(){
+        new PowerUpCard(null, validName, validDescription, validColor);
+    }
+    @Test(expected = NullPointerException.class)
+    public void createPowerUpCard_nullNameProvided_shouldThrowNullPointerException(){
+        new PowerUpCard(validId, null, validDescription, validColor);
+    }
+    @Test(expected = NullPointerException.class)
+    public void createPowerUpCard_nullDescriptionProvided_shouldThrowNullPointerException(){
+        new PowerUpCard(validId, validName, null, validColor);
+    }
+    @Test(expected = NullPointerException.class)
+    public void createPowerUpCard_nullColorProvided_shouldThrowNullPointerException(){
+        new PowerUpCard(validId, validName, validDescription, null);
+    }
+
+    @Test
+    public void createPowerUpCard_validParametersProvided_shouldNotThrowException(){
+        new PowerUpCard(validId, validName, validDescription, validColor);
+    }
+
+    @Test
+    public void getId_shouldReturnProvidedId(){
+        PowerUpCard validPowerUpCard = new PowerUpCard(validId, validName, validDescription, validColor);
+        assertEquals(validPowerUpCard.getId(), validId);
+    }
+
+    @Test
+    public void getName_shouldReturnProvidedName(){
+        PowerUpCard validPowerUpCard = new PowerUpCard(validId, validName, validDescription, validColor);
+        assertEquals(validPowerUpCard.getName(), validName);
+    }
+
+    @Test
+    public void getDescription_shouldReturnProvidedDescription(){
+        PowerUpCard validPowerUpCard = new PowerUpCard(validId, validName, validDescription, validColor);
+        assertEquals(validPowerUpCard.getDescription(), validDescription);
+    }
+
+    @Test
+    public void getColor_shouldReturnProvidedColor(){
+        PowerUpCard validPowerUpCard = new PowerUpCard(validId, validName, validDescription, validColor);
+        assertEquals(validPowerUpCard.getCubeColor(), validColor);
+    }
+
+    @Test
+    public void checkEquals_selfPassed_shouldReturnTrue(){
+        PowerUpCard validPowerUpCard = new PowerUpCard(validId, validName, validDescription, validColor);
+        assertTrue(validPowerUpCard.equals(validPowerUpCard));
+    }
+
+    @Test
+    public void checkEquals_anotherClassPassed_shouldreturnFalse(){
+        PowerUpCard validPowerUpCard = new PowerUpCard(validId, validName, validDescription, validColor);
+        assertFalse(validPowerUpCard.equals(new Object()));
+    }
+
+    @Test
+    public void checkEquals_PowerUpCardWithSameIdAndColorProvided_shouldReturnTrue(){
+        PowerUpCard validPowerUpCard = new PowerUpCard(validId, validName, validDescription, validColor);
+        assertTrue(validPowerUpCard.equals(new PowerUpCard(validId, "Another card", "Another description", validColor)));
+    }
+
+    @Test
+    public void checkEquals_PowerUpCardWithSameIdAndDifferentColorProvided_shouldReturnFalse(){
+        PowerUpCard validPowerUpCard = new PowerUpCard(validId, validName, validDescription, validColor);
+        assertFalse(validPowerUpCard.equals(new PowerUpCard(validId, "Another card", "Another description", AmmoCube.YELLOW)));
+    }
+
+    @Test
+    public void checkEquals_PowerUpCardWithDifferentIdAndSameColorProvided_shouldReturnFalse(){
+        PowerUpCard validPowerUpCard = new PowerUpCard(validId, validName, validDescription, validColor);
+        assertFalse(validPowerUpCard.equals(new PowerUpCard("anotherid", "Another card", "Another description", validColor)));
+    }
+
+    @Test
+    public void checkEquals_PowerUpCardWithDifferentIdAndColorProvided_shouldReturnFalse(){
+        PowerUpCard validPowerUpCard = new PowerUpCard(validId, validName, validDescription, validColor);
+        assertFalse(validPowerUpCard.equals(new PowerUpCard("anotherid", validName, validDescription, AmmoCube.YELLOW)));
+    }
+
+    @Test
+    public void getHashCode_compareWithAnotherCard_ShouldBeSame(){
+        PowerUpCard validPowerUpCard = new PowerUpCard(validId, validName, validDescription, validColor);
+        PowerUpCard anotherCard = new PowerUpCard("anotherid", "anothername", "anotherdescription", AmmoCube.YELLOW);
+        assertNotEquals(validPowerUpCard.hashCode(), anotherCard.hashCode());
+    }
+
+    @Test
+    public void serializeCard_shouldContainAllInformation(){
+        PowerUpCard validPowerUpCard = new PowerUpCard(validId, validName, validDescription, validColor);
+        try {
+            final String serializedPowerUpCard = mapper.writeValueAsString(validPowerUpCard);
+            assertThat(serializedPowerUpCard, containsString(String.format("\"id\":\"%s\"", validId)));
+            assertThat(serializedPowerUpCard, containsString(String.format("\"name\":\"%s\"", validName)));
+            assertThat(serializedPowerUpCard, containsString(String.format("\"description\":\"%s\"", validDescription)));
+            assertThat(serializedPowerUpCard, containsString(String.format("\"cubeColor\":\"%s\"", validColor.name())));
+        } catch (JsonProcessingException e) {
+            fail();
+        }
+    }
+
+}
