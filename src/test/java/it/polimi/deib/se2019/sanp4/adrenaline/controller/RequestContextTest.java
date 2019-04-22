876e353ab912eff231b0820fa88581d94c5047ea
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2019-04-22 16:00:59 +0200

    Add tests for RequestContext

---
+++ src/test/java/it/polimi/deib/se2019/sanp4/adrenaline/controller/RequestContextTest.java
@@ -0,0 +1,46 @@
+package it.polimi.deib.se2019.sanp4.adrenaline.controller;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RequestContextTest {
+
+    private static ChoiceHandler<Object> validHandler;
+    private static Request<Object> validRequest;
+
+    @BeforeClass
+    public static void setup(){
+        validHandler = new ChoiceHandler<Object>() {
+            @Override
+            public void handleChoice(Object choice) {}
+
+            @Override
+            public void cancel() {}
+        };
+        validRequest = new Request<Object>() {
+            @Override
+            public boolean isValid(Object choice) {
+                return true;
+            }
+        };
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void createContext_nullRequestProvided_shouldThrowNullPointerException(){
+        new RequestContext<>(null, validHandler);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void createContext_nullHandlerProvided_shouldThrowNullPointerException(){
+        new RequestContext<>(validRequest, null);
+    }
+
+    @Test
+    public void createContext_properParametersProvided_shouldNotThrowException(){
+        RequestContext<Object> context = new RequestContext<>(validRequest, validHandler);
+        assertEquals(validRequest, context.getRequest());
+        assertEquals(validHandler, context.getHandler());
+    }
+}
