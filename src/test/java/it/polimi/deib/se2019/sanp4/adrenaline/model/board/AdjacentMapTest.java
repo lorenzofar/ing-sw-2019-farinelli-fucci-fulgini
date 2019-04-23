212df6f6f05ac57066c032e9ba5172d64dcea9f6
tizianofucci <tizfcc@gmail.com>
2019-04-23 11:08:19 +0200

    Fix tests for AdjacentMap

--- src/test/java/it/polimi/deib/se2019/sanp4/adrenaline/model/board/AdjacentMapTest.java
+++ src/test/java/it/polimi/deib/se2019/sanp4/adrenaline/model/board/AdjacentMapTest.java
@@ -1,17 +1,28 @@
 package it.polimi.deib.se2019.sanp4.adrenaline.model.board;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Map;
+import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
 public class AdjacentMapTest {
 
+    @Test (expected = NullPointerException.class)
+    public void getConnection_NullDirectionProvided_ShouldThrowNullPointerException() {
+        Map<CardinalDirection, SquareConnection> map = new HashMap<CardinalDirection, SquareConnection>();
+        CoordPair coordPair = new CoordPair(5,5);
+        SquareConnection squareConnection = new SquareConnection(coordPair, SquareConnectionType.DOOR);
+        map.put(CardinalDirection.N, squareConnection);
+        AdjacentMap adjacentMap = new AdjacentMap(map);
+
+        adjacentMap.getConnection(null);
+    }
+
     @Test
-    public void getConnection() {
+    public void getConnection_SingleDirectionMap_getConnectionShouldReturnSameConnection() {
+        
     }
 
     @Test
@@ -24,7 +35,21 @@ public class AdjacentMapTest {
         new AdjacentMap(map);
     }
 
-    //TODO: test the second "if" and the final statement
+    //TODO: check this test after modifying AdjacentMap
+    @Test(expected = NullPointerException.class)
+    public void createMap_mapWithEmptyValuesProvided_ShouldThrowNullPointerException(){
+        Map<CardinalDirection, SquareConnection> map = new HashMap<CardinalDirection, SquareConnection>();
+        map.put(null, null);
+        new AdjacentMap(map);
+    }
 
-
+    @Test
+    public void createMap_mapWithOneValueProvided_ShouldNotThrowException(){
+        Map<CardinalDirection, SquareConnection> map = new HashMap<CardinalDirection, SquareConnection>();
+        CoordPair coordPair = new CoordPair(5,5);
+        SquareConnection squareConnection = new SquareConnection(coordPair, SquareConnectionType.DOOR);
+        map.put(CardinalDirection.N, squareConnection);
+        AdjacentMap adjacentMap = new AdjacentMap(map);
+        assertEquals(adjacentMap.getConnection(CardinalDirection.N), squareConnection);
+    }
 }
