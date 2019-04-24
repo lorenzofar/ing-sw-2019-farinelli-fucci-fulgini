87b84fad3b86fd2d057886aa5406252aebc02929
Alessandro Fulgini <fuljo97@gmail.com>
2019-04-24 19:21:27 +0200

    Make WeaponCard constructor public

--- src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/model/items/weapons/WeaponCard.java
+++ src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/model/items/weapons/WeaponCard.java
@@ -35,7 +35,7 @@ public class WeaponCard {
      * @param name The name of the weapon, not null and not an empty string
      * @param cost The list of objects representing the cost of the weapon, not null and not empty
      */
-    WeaponCard(String id, String name, List<AmmoCubeCost> cost, List<EffectDescription> effects){
+    public WeaponCard(String id, String name, List<AmmoCubeCost> cost, List<EffectDescription> effects){
         if(id == null || name == null || cost == null){
             throw new NullPointerException("Found null parameters");
         }
