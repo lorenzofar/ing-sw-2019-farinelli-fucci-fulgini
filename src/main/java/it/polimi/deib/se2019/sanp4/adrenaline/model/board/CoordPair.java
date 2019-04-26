2a8fb1924933c1a00286d70d878ebff1a600166f
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2019-04-26 12:06:18 +0200

    Add equals method to CoordPair
    Overriden equals method to make two coord pair be equal when they reference the same point on the grid (i.e. same x and y coordinate).
    Added some tests to test the newly added methods

--- src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/model/board/CoordPair.java
+++ src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/model/board/CoordPair.java
@@ -1,5 +1,7 @@
 package it.polimi.deib.se2019.sanp4.adrenaline.model.board;
 
+import java.util.Objects;
+
 /** A class representing a location in cartesian coordinates */
 public class CoordPair {
     /** The X coordinate */
@@ -37,6 +39,18 @@ public class CoordPair {
      */
     public int getY(){
         return this.y;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (obj == this) return true;
+        if(!(obj instanceof CoordPair)) return false;
+        return ((CoordPair) obj).getX() == this.x && ((CoordPair)obj).getY() == this.y;
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(x, y);
     }
 
 }
--- src/test/java/it/polimi/deib/se2019/sanp4/adrenaline/model/board/CoordPairTest.java
+++ src/test/java/it/polimi/deib/se2019/sanp4/adrenaline/model/board/CoordPairTest.java
@@ -25,4 +25,46 @@ public class CoordPairTest {
         assertEquals(y, coordPair.getY());
     }
 
+    @Test
+    public void checkEquals_selfPassed_shouldBeEqual(){
+        CoordPair pair = new CoordPair(1, 2);
+        assertEquals(pair, pair);
+    }
+
+    @Test
+    public void checkEquals_anotherClassPassed_shouldNotBeEqual(){
+        CoordPair pair = new CoordPair(1, 2);
+        assertNotEquals(new Object(), pair);
+    }
+
+    @Test
+    public void checkEquals_coordPairWithSameValuesPassed_shouldBeEqual(){
+        int x = 5;
+        int y = 5;
+        CoordPair pair1 = new CoordPair(x, y);
+        CoordPair pair2 = new CoordPair(x, y);
+        assertEquals(pair1, pair2);
+    }
+
+    @Test
+    public void checkEquals_coordPairWithDifferentValuesPassed_shouldNotBeEqual(){
+        CoordPair pair1 = new CoordPair(1, 2);
+        CoordPair pair2 = new CoordPair(2, 1);
+        assertNotEquals(pair1, pair2);
+    }
+
+    @Test
+    public void checkHashCode_compareWithDifferentCoordPair_shouldBeDifferent(){
+        CoordPair pair1 = new CoordPair(1, 2);
+        CoordPair pair2 = new CoordPair(2, 1);
+        assertNotEquals(pair1.hashCode(), pair2.hashCode());
+    }
+
+    @Test
+    public void checkHashCode_compareWithSameCoordPair_shouldBeSame(){
+        CoordPair pair1 = new CoordPair(1, 2);
+        CoordPair pair2 = new CoordPair(1, 2);
+        assertEquals(pair1.hashCode(), pair2.hashCode());
+    }
+
 }
