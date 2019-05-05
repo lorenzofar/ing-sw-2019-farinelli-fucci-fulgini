44b2fe652652ad52414d64462f5b7aa88e896de3
tizianofucci <tizfcc@gmail.com>
2019-05-05 16:50:52 +0200

    Add tests for RemoteObservable and RemoteObserver

---
+++ src/test/java/it/polimi/deib/se2019/sanp4/adrenaline/common/observer/RemoteObservableObserverTest.java
@@ -0,0 +1,52 @@
+package it.polimi.deib.se2019.sanp4.adrenaline.common.observer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RemoteObservableObserverTest {
+
+    private String message = "Message";
+
+    public class RemoteObservableImpl extends RemoteObservable<String>{
+        RemoteObservableImpl(){
+            super();
+        }
+    }
+
+    public class RemoteObserverImpl implements RemoteObserver<String> {
+
+        boolean shouldReceiveUpdate;
+
+        @Override
+        public void update(String event) {
+            if (shouldReceiveUpdate) {
+                assertEquals(message, event);
+            } else {
+                fail();
+            }
+        }
+    }
+
+    @Test
+    public void addRemoteObserver_Notify_ShouldSucceed(){
+        RemoteObservableImpl remoteObservable = new RemoteObservableImpl();
+        RemoteObserverImpl remoteObserver = new RemoteObserverImpl();
+        remoteObservable.addObserver(remoteObserver);
+        remoteObserver.shouldReceiveUpdate = true;
+        remoteObservable.notifyObservers(message);
+    }
+
+    @Test
+    public void removeObserver_ShouldSucceed(){
+        RemoteObservableImpl remoteObservable = new RemoteObservableImpl();
+        RemoteObserverImpl remoteObserver = new RemoteObserverImpl();
+        remoteObservable.addObserver(remoteObserver);
+        remoteObserver.shouldReceiveUpdate = true;
+        remoteObservable.notifyObservers(message);
+        remoteObservable.removeObserver(remoteObserver);
+        remoteObserver.shouldReceiveUpdate = false;
+        remoteObservable.notifyObservers(message);
+    }
+
+}
