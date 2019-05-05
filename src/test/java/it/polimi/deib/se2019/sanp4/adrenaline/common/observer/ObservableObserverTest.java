54745355b7626af05faea3e6f45dd974e61ca106
tizianofucci <tizfcc@gmail.com>
2019-05-05 13:56:57 +0200

    Add tests for Observable and Observer

---
+++ src/test/java/it/polimi/deib/se2019/sanp4/adrenaline/common/observer/ObservableObserverTest.java
@@ -0,0 +1,52 @@
+package it.polimi.deib.se2019.sanp4.adrenaline.common.observer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ObservableObserverTest {
+
+    private String message = "Message";
+
+    public class ObservableImpl extends Observable<String>{
+        ObservableImpl(){
+            super();
+        }
+    }
+
+    public class ObserverImpl implements Observer<String> {
+
+        boolean shouldReceiveUpdate;
+
+        @Override
+        public void update(String event) {
+            if (shouldReceiveUpdate) {
+                assertEquals(message, event);
+            } else {
+                fail();
+            }
+        }
+    }
+
+    @Test
+    public void addObserver_Notify_ShouldSucceed(){
+        ObservableImpl observableImpl = new ObservableImpl();
+        ObserverImpl observer = new ObserverImpl();
+        observableImpl.addObserver(observer);
+        observer.shouldReceiveUpdate = true;
+        observableImpl.notifyObservers(message);
+    }
+
+    @Test
+    public void removeObserver_ShouldSucceed(){
+        ObservableImpl observableImpl = new ObservableImpl();
+        ObserverImpl observer = new ObserverImpl();
+        observableImpl.addObserver(observer);
+        observer.shouldReceiveUpdate = true;
+        observableImpl.notifyObservers(message);
+        observableImpl.removeObserver(observer);
+        observer.shouldReceiveUpdate = false;
+        observableImpl.notifyObservers(message);
+    }
+
+}
