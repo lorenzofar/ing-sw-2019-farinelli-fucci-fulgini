666c470d2e8a6d497e5d1a37218d8c135bb67fc2
tizianofucci <tizfcc@gmail.com>
2019-05-07 17:43:22 +0200

    Add tests for RemoteRoutingObservable

--- src/test/java/it/polimi/deib/se2019/sanp4/adrenaline/common/observer/RemoteObservableObserverTest.java
+++ src/test/java/it/polimi/deib/se2019/sanp4/adrenaline/common/observer/RemoteObservableObserverTest.java
@@ -2,6 +2,8 @@ package it.polimi.deib.se2019.sanp4.adrenaline.common.observer;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class RemoteObservableObserverTest {
@@ -28,7 +30,7 @@ public class RemoteObservableObserverTest {
         }
     }
 
-    public class RemoteRoutingObservableImpl extends RoutingObservable<String>{
+    public class RemoteRoutingObservableImpl extends RemoteRoutingObservable<String>{
         RemoteRoutingObservableImpl(){
             super();
         }
@@ -57,6 +59,59 @@ public class RemoteObservableObserverTest {
         remoteObservable.notifyObservers(message);
     }
 
+    @Test
+    public void remoteRoutingAddObserver_Notify_ShouldSucceed(){
+        RemoteObserverImpl remoteObserver = new RemoteObserverImpl();
+        RemoteRoutingObservableImpl remoteRoutingObservable = new RemoteRoutingObservableImpl();
+        remoteRoutingObservable.addObserver("Giammarco", remoteObserver);
+        remoteObserver.shouldReceiveUpdate = true;
+        remoteRoutingObservable.notifyObservers(message);
+    }
 
+    @Test
+    public void remoteRoutingRemoveObserver_ShouldSucceed(){
+        RemoteObserverImpl remoteObserver = new RemoteObserverImpl();
+        RemoteRoutingObservableImpl remoteRoutingObservable = new RemoteRoutingObservableImpl();
+        remoteRoutingObservable.addObserver("Giammarco", remoteObserver);
+        remoteRoutingObservable.removeObserver("Giammarco", remoteObserver);
+        remoteRoutingObservable.removeObserver("Giammarco", remoteObserver);
+        remoteObserver.shouldReceiveUpdate = false;
+        remoteRoutingObservable.notifyObservers(message);
+    }
+
+    @Test
+    public void remoteRoutingRemoveAllObservers_ShouldSucceed(){
+        RemoteObserverImpl remoteObserver = new RemoteObserverImpl();
+        RemoteObserverImpl remoteObserver2 = new RemoteObserverImpl();
+        RemoteRoutingObservableImpl remoteRoutingObservable = new RemoteRoutingObservableImpl();
+        remoteRoutingObservable.addObserver("Giammarco", remoteObserver);
+        remoteRoutingObservable.addObserver("Giammarco", remoteObserver2);
+        remoteRoutingObservable.removeAllObservers("Giammarco");
+        remoteObserver.shouldReceiveUpdate = false;
+        remoteObserver2.shouldReceiveUpdate = false;
+        remoteRoutingObservable.notifyObservers(message);
+    }
+
+    @Test
+    public void remoteRoutingAddObserver_NotifyUser_ShouldSucceed(){
+        RemoteObserverImpl remoteObserver = new RemoteObserverImpl();
+        RemoteRoutingObservableImpl remoteRoutingObservable = new RemoteRoutingObservableImpl();
+        remoteRoutingObservable.addObserver("Giammarco", remoteObserver);
+        remoteObserver.shouldReceiveUpdate = true;
+        remoteRoutingObservable.notifyObservers("Giammarco", message);
+    }
+
+    @Test
+    public void routingNotifyObservers_ShouldSucceed(){
+        RemoteObserverImpl remoteObserverG = new RemoteObserverImpl();
+        RemoteObserverImpl remoteObserverS = new RemoteObserverImpl();
+        RemoteRoutingObservableImpl remoteRoutingObservable = new RemoteRoutingObservableImpl();
+        remoteRoutingObservable.addObserver("Giammarco", remoteObserverG);
+        remoteRoutingObservable.addObserver("Stefano", remoteObserverG);
+        remoteObserverG.shouldReceiveUpdate = true;
+        remoteObserverS.shouldReceiveUpdate = true;
+        remoteRoutingObservable.notifyObservers(Arrays.asList("Stefano", "Giammarco", "German"), message);
+
+    }
 
 }
