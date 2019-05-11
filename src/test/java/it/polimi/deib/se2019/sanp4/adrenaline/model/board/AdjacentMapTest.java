043cb0b53d2100392748e616f7e19c37a110154b
tizianofucci <tizfcc@gmail.com>
2019-05-11 20:38:42 +0200

    Add tests for Board

--- src/test/java/it/polimi/deib/se2019/sanp4/adrenaline/model/board/AdjacentMapTest.java
+++ src/test/java/it/polimi/deib/se2019/sanp4/adrenaline/model/board/AdjacentMapTest.java
@@ -14,6 +14,26 @@ public class AdjacentMapTest {
         adjacentMap.getConnection(null); /* This will throw */
     }
 
+    @Test (expected = NullPointerException.class)
+    public void setConnection_NullDirectionProvided_ShouldThrowNullPointerException() {
+        AdjacentMap adjacentMap = new AdjacentMap();
+        adjacentMap.setConnection(null, new CoordPair(5,5), SquareConnectionType.DOOR);
+    }
+
+    @Test
+    public void setConnection_ShouldReturnSameConnection() {
+        AdjacentMap adjacentMap = new AdjacentMap();
+        adjacentMap.setConnection(CardinalDirection.N, new CoordPair(5,5), SquareConnectionType.DOOR);
+        assertEquals(SquareConnectionType.DOOR, adjacentMap.getConnection(CardinalDirection.N).getConnectionType());
+    }
+
+    @Test
+    public void getSquares_CollectionShouldReturnBothSquares(){
+        AdjacentMap adjacentMap = new AdjacentMap();
+        adjacentMap.setConnection(CardinalDirection.N, new CoordPair(5,5), SquareConnectionType.DOOR);
+
+    }
+
     @Ignore
     public void getConnection_SingleDirectionMap_getConnectionShouldReturnSameConnection() {
         fail();
---
+++ src/test/java/it/polimi/deib/se2019/sanp4/adrenaline/model/board/BoardTest.java
@@ -0,0 +1,43 @@
+package it.polimi.deib.se2019.sanp4.adrenaline.model.board;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BoardTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void createBoard_NegativeValueProvided_ShouldThrowIllegalArgumentException(){
+        Board board = new Board(-1, 0);
+    }
+
+    @Test
+    public void createBoard_ShouldSucceed(){
+        Board board = new Board(4,3);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void addSquare_NullSquareProvided_ShouldThrowNullPointerException(){
+        Board board = new Board(4,3);
+        board.addSquare(null);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void addSquare_SquareOutsideBoardProvided_ShouldThrowNullPointerException(){
+        Board board = new Board(4,3);
+        CoordPair location = new CoordPair(5,3);
+        board.addSquare(new AmmoSquare(location));
+    }
+
+    @Test
+    public void addSquare_ValidSquareProvided_PreviousSquareExistent_ShouldSucceed(){
+        Board board = new Board(4,3);
+        CoordPair location = new CoordPair(3,2);
+        board.addSquare(new AmmoSquare(location));
+        board.addSquare(new AmmoSquare(location));
+    }
+
+
+
+
+}
