c4ed6c3d021ab5d74d9ac3a49c9d84e2da88b9ce
Alessandro Fulgini <fuljo97@gmail.com>
2019-05-12 17:33:31 +0200

    Add new BoardNotFoundException

---
+++ src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/common/exceptions/BoardNotFoundException.java
@@ -0,0 +1,11 @@
+package it.polimi.deib.se2019.sanp4.adrenaline.common.exceptions;
+
+public class BoardNotFoundException extends Exception {
+    public BoardNotFoundException() {
+        super();
+    }
+
+    public BoardNotFoundException(String message) {
+        super(message);
+    }
+}
