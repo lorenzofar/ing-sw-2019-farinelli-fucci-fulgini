68f4a8efb0493830ee77044f2841b413ffe45914
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2019-05-19 18:14:37 +0200

    Add corresponding ANSI and HEX codes to colors enumerator

--- src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/model/board/RoomColor.java
+++ src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/model/board/RoomColor.java
@@ -1,28 +1,54 @@
 package it.polimi.deib.se2019.sanp4.adrenaline.model.board;
 
-/** Represents the possible colors a room can have */
+/**
+ * Represents the possible colors a room can have
+ */
 public enum RoomColor {
-    /** Blue color */
-    BLUE("Blue"),
-    /** Red color */
-    RED("Red"),
-    /** Gray color */
-    GRAY("Gray"),
-    /** Yellow color */
-    YELLOW("Yellow"),
-    /** Purple color */
-    PURPLE("Purple"),
-    /** Green color */
-    GREEN("Green");
+    /**
+     * Blue color
+     */
+    BLUE("Blue", "\u001B[34m", "#3779ff"),
+    /**
+     * Red color
+     */
+    RED("Red", "\u001B[31m", "#f63a30"),
+    /**
+     * Gray color
+     */
+    GRAY("Gray", "\u001B[37m", "#d6d6d6"),
+    /**
+     * Yellow color
+     */
+    YELLOW("Yellow", "\u001B[33m", "#f9cc00"),
+    /**
+     * Purple color
+     */
+    PURPLE("Purple", "\u001B[35m", "#5856d6"),
+    /**
+     * Green color
+     */
+    GREEN("Green", "\u001B[32m", "#4cd963");
 
     private String message;
+    private String ansiCode;
+    private String hexCode;
 
-    RoomColor(String message){
+    RoomColor(String message, String ansiCode, String hexCode) {
         this.message = message;
+        this.ansiCode = ansiCode;
+        this.hexCode = hexCode;
     }
 
     @Override
-    public String toString(){
-        return this.message;
+    public String toString() {
+        return message;
+    }
+
+    public String getANSICode() {
+        return ansiCode;
+    }
+
+    public String getHexCode() {
+        return hexCode;
     }
 }
--- src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/model/player/PlayerColor.java
+++ src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/model/player/PlayerColor.java
@@ -1,26 +1,50 @@
 package it.polimi.deib.se2019.sanp4.adrenaline.model.player;
 
-/** Represents the possible colors a player can have */
+/**
+ * Represents the possible colors a player can have
+ */
 public enum PlayerColor {
-    /** Blue color */
-    BLUE("Blue"),
-    /** Gray color */
-    GRAY("Gray"),
-    /** Yellow color */
-    YELLOW("Yellow"),
-    /** Purple color */
-    PURPLE("Purple"),
-    /** Green color */
-    GREEN("Green");
+    /**
+     * Blue color
+     */
+    BLUE("Blue", "\u001B[34m", "#3779ff"),
+    /**
+     * Gray color
+     */
+    GRAY("Gray", "\u001B[37m", "#d6d6d6"),
+    /**
+     * Yellow color
+     */
+    YELLOW("Yellow", "\u001B[33m", "#f9cc00"),
+    /**
+     * Purple color
+     */
+    PURPLE("Purple", "\u001B[35m", "#5856d6"),
+    /**
+     * Green color
+     */
+    GREEN("Green", "\u001B[32m", "#4cd963");
 
     private String message;
+    private String ansiCode;
+    private String hexCode;
 
-    PlayerColor(String message){
+    PlayerColor(String message, String ansiCode, String hexCode) {
         this.message = message;
+        this.ansiCode = ansiCode;
+        this.hexCode = hexCode;
     }
 
     @Override
-    public String toString(){
-        return this.message;
+    public String toString() {
+        return message;
+    }
+
+    public String getANSICode() {
+        return ansiCode;
+    }
+
+    public String getHexCode() {
+        return hexCode;
     }
 }
