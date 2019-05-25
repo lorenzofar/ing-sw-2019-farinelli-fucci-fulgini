78719d7ffbbfd8b7ea5435cb5a86586aba596f8a
tizianofucci <tizfcc@gmail.com>
2019-05-25 15:10:30 +0200

    Add BoardUpdateTest

--- src/test/java/it/polimi/deib/se2019/sanp4/adrenaline/common/updates/BoardUpdateTest.java
+++ src/test/java/it/polimi/deib/se2019/sanp4/adrenaline/common/updates/BoardUpdateTest.java
@@ -1,18 +1,54 @@
 package it.polimi.deib.se2019.sanp4.adrenaline.common.updates;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import it.polimi.deib.se2019.sanp4.adrenaline.common.modelviews.AmmoSquareView;
+import it.polimi.deib.se2019.sanp4.adrenaline.common.modelviews.BoardView;
+import it.polimi.deib.se2019.sanp4.adrenaline.common.modelviews.SpawnSquareView;
+import it.polimi.deib.se2019.sanp4.adrenaline.common.modelviews.SquareView;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.board.CoordPair;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.board.RoomColor;
 import it.polimi.deib.se2019.sanp4.adrenaline.utils.JSONUtils;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 
 public class BoardUpdateTest {
 
     private ObjectMapper objectMapper = JSONUtils.getObjectMapper();
+    private BoardView board;
+    private int rows = 2;
+    private int columns = 1;
+    private SquareView[][] squares = new SquareView[columns][rows];
+
 
 
     @Test
-    public void serialize_ShouldSucceed() {
+    public void serialize_ShouldSucceed() throws IOException {
+        board = new BoardView(rows,columns);
+        AmmoSquareView ammoSquareView = new AmmoSquareView(new CoordPair(0,0),
+                                        RoomColor.BLUE);
+        SpawnSquareView spawnSquareView = new SpawnSquareView(new CoordPair(0,1),
+                                        RoomColor.BLUE);
+        squares[0][0] = ammoSquareView;
+        squares[0][1] = spawnSquareView;
+        board.setSquares(squares);
+
+        BoardUpdate update = new BoardUpdate(board);
+        String s = objectMapper.writeValueAsString(update);
+
+        BoardUpdate boardUpdate = objectMapper.readValue(s, BoardUpdate.class);
+
+        assertEquals(rows, boardUpdate.getBoard().printRowsCount());
+        assertEquals(columns, boardUpdate.getBoard().printColumnsCount());
+        assertEquals(squares, boardUpdate.getBoard().getSquares());
+        assertEquals(ammoSquareView, boardUpdate.getBoard().getSquare(new CoordPair(0,0)));
+        assertEquals(spawnSquareView, boardUpdate.getBoard().getSquare(new CoordPair(0,1)));
 
 
     }
