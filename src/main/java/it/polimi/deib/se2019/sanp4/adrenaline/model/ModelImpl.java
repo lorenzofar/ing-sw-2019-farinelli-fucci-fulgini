e70b1698db768eeac7363c7a2439eedd35e05daf
Alessandro Fulgini <fuljo97@gmail.com>
2019-05-25 15:14:35 +0200

    Refactor Model and ModelImpl

--- src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/model/ModelImpl.java
+++ src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/model/ModelImpl.java
@@ -1,5 +1,6 @@
 package it.polimi.deib.se2019.sanp4.adrenaline.model;
 
+import it.polimi.deib.se2019.sanp4.adrenaline.common.observer.Observable;
 import it.polimi.deib.se2019.sanp4.adrenaline.common.observer.RemoteRoutingObservable;
 import it.polimi.deib.se2019.sanp4.adrenaline.model.match.Match;
 import it.polimi.deib.se2019.sanp4.adrenaline.common.updates.ModelUpdate;
@@ -16,8 +17,7 @@ public class ModelImpl extends RemoteRoutingObservable<ModelUpdate> implements M
      * Creates an empty model.
      * The initial operational state is {@link MatchOperationalState#WAITING_PLAYERS}
      */
-    ModelImpl(){
-        //TODO: Finish the constructor
+    public ModelImpl(){
         operationalState = MatchOperationalState.WAITING_PLAYERS;
     }
 
@@ -32,14 +32,26 @@ public class ModelImpl extends RemoteRoutingObservable<ModelUpdate> implements M
     }
 
     /**
-     * Sets the current match to given match
-     * @param match match instance
+     * Sets the internal instance to the match
+     *
+     * @param match the match to be set, not null
      */
-    void setMatch(Match match) {
-        if(match == null){
-            throw new NullPointerException("Match cannot be null");
-        }
+    @Override
+    public void setMatch(Match match) {
         this.match = match;
+    }
+
+    /**
+     * Suspends a player if the player exists and the match has been created,
+     * otherwise it does nothing
+     *
+     * @param username the username of the player to be suspended
+     */
+    @Override
+    public void suspendPlayer(String username) {
+        if (match != null && username != null) {
+            match.suspendPlayer(username);
+        }
     }
 
     /**
@@ -50,5 +62,26 @@ public class ModelImpl extends RemoteRoutingObservable<ModelUpdate> implements M
     @Override
     public MatchOperationalState getOperationalState() {
         return operationalState;
+    }
+
+    /**
+     * Sets the operational state of the match
+     *
+     * @param state the operational state
+     */
+    @Override
+    public void setOperationalState(MatchOperationalState state) {
+        this.operationalState = state;
+    }
+
+    /**
+     * Send an update/event from an {@link Observable} object.
+     *
+     * @param event event/update to be sent
+     */
+    @Override
+    public void update(ModelUpdate event) {
+        /* Receive updates from the other classes of the model */
+        /* TODO: Implement this method */
     }
 }
