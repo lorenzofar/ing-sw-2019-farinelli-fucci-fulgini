5de889fa50bb1d19a8aeddb24d9a84a8a745e313
tizianofucci <tizfcc@gmail.com>
2019-05-25 14:03:58 +0200

    Add tests for SpawnSquareView in SquareUpdateTest

--- src/test/java/it/polimi/deib/se2019/sanp4/adrenaline/common/updates/SquareUpdateTest.java
+++ src/test/java/it/polimi/deib/se2019/sanp4/adrenaline/common/updates/SquareUpdateTest.java
@@ -2,6 +2,7 @@ package it.polimi.deib.se2019.sanp4.adrenaline.common.updates;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.polimi.deib.se2019.sanp4.adrenaline.common.modelviews.AmmoSquareView;
+import it.polimi.deib.se2019.sanp4.adrenaline.common.modelviews.SpawnSquareView;
 import it.polimi.deib.se2019.sanp4.adrenaline.model.board.CardinalDirection;
 import it.polimi.deib.se2019.sanp4.adrenaline.model.board.CoordPair;
 import it.polimi.deib.se2019.sanp4.adrenaline.model.board.RoomColor;
@@ -67,6 +68,49 @@ public class SquareUpdateTest {
         ammoSquareUpdate = objectMapper.readValue(s, SquareUpdate.class);
 
         assertEquals(players, ammoSquareUpdate.getSquare().getPlayers());
+
+    }
+
+    @Test
+    public void spawnSquareView_Serialize_ShouldSucceed() throws IOException {
+        SpawnSquareView spawnSquareView = new SpawnSquareView(location, roomColor);
+
+        /* Test setters */
+        players.add("Lorenzo");
+        players.add("Alessandro");
+        spawnSquareView.setPlayers(players);
+        adjacentMap.put(CardinalDirection.N, SquareConnectionType.DOOR);
+        adjacentMap.put(CardinalDirection.S, SquareConnectionType.DOOR);
+        adjacentMap.put(CardinalDirection.W, SquareConnectionType.WALL);
+        adjacentMap.put(CardinalDirection.E, SquareConnectionType.FLOOR);
+        spawnSquareView.setAdjacentMap(adjacentMap);
+
+        /* Create update and serialize */
+        SquareUpdate update = new SquareUpdate(spawnSquareView);
+        String s = objectMapper.writeValueAsString(update);
+
+        SquareUpdate spawnSquareUpdate = objectMapper.readValue(s, SquareUpdate.class);
+
+        assertEquals(players, spawnSquareUpdate.getSquare().getPlayers());
+        assertEquals(roomColor, spawnSquareUpdate.getSquare().getRoomColor());
+        assertEquals(adjacentMap, spawnSquareUpdate.getSquare().getAdjacentMap());
+
+        /* Test addPlayer and removePlayer */
+        players.add("Tiziano");
+        spawnSquareView.addPlayer("Tiziano");
+        update = new SquareUpdate(spawnSquareView);
+        s = objectMapper.writeValueAsString(update);
+        spawnSquareUpdate = objectMapper.readValue(s, SquareUpdate.class);
+
+        assertEquals(players, spawnSquareUpdate.getSquare().getPlayers());
+
+        players.remove("Tiziano");
+        spawnSquareView.removePlayer("Tiziano");
+        update = new SquareUpdate(spawnSquareView);
+        s = objectMapper.writeValueAsString(update);
+        spawnSquareUpdate = objectMapper.readValue(s, SquareUpdate.class);
+
+        assertEquals(players, spawnSquareUpdate.getSquare().getPlayers());
 
     }
 }
