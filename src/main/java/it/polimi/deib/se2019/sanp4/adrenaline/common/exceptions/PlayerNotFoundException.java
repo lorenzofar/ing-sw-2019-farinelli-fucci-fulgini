5a8c4c47d5a64532557754f56100fe29fed88ac0
Alessandro Fulgini <fuljo97@gmail.com>
2019-05-26 20:10:42 +0200

    Make PlayerNotFoundException a runtime exception

--- src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/common/exceptions/PlayerNotFoundException.java
+++ src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/common/exceptions/PlayerNotFoundException.java
@@ -4,7 +4,7 @@ package it.polimi.deib.se2019.sanp4.adrenaline.common.exceptions;
  * An exception used when an operation targeting a player fails
  * because the player cannot be affected by it (e.g. removing a player from a square)
  */
-public class PlayerNotFoundException extends Exception {
+public class PlayerNotFoundException extends RuntimeException {
 
     private static final long serialVersionUID = 6751640880343681499L;
 
