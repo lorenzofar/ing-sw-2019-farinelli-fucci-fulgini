6d02b310e51e71c09a91e60f3517c195193ddfdd
tizianofucci <tizfcc@gmail.com>
2019-05-29 15:24:34 +0200

    Add test for generateView in SpawnSquare

--- src/test/java/it/polimi/deib/se2019/sanp4/adrenaline/model/board/SpawnSquareTest.java
+++ src/test/java/it/polimi/deib/se2019/sanp4/adrenaline/model/board/SpawnSquareTest.java
@@ -2,17 +2,23 @@ package it.polimi.deib.se2019.sanp4.adrenaline.model.board;
 
 import it.polimi.deib.se2019.sanp4.adrenaline.common.exceptions.CardNotFoundException;
 import it.polimi.deib.se2019.sanp4.adrenaline.common.exceptions.FullCapacityException;
+import it.polimi.deib.se2019.sanp4.adrenaline.common.modelviews.AmmoSquareView;
+import it.polimi.deib.se2019.sanp4.adrenaline.common.modelviews.SpawnSquareView;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.ModelTestUtil;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.items.ammo.AmmoCard;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.items.ammo.AmmoCube;
 import it.polimi.deib.se2019.sanp4.adrenaline.model.items.ammo.AmmoCubeCost;
 import it.polimi.deib.se2019.sanp4.adrenaline.model.items.weapons.EffectDescription;
 import it.polimi.deib.se2019.sanp4.adrenaline.model.items.weapons.WeaponCard;
 import it.polimi.deib.se2019.sanp4.adrenaline.model.match.Match;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.match.MatchConfiguration;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.match.MatchCreator;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.player.Player;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -20,6 +26,23 @@ import static org.junit.Assert.*;
 
 public class SpawnSquareTest {
     private static SpawnSquare square;
+    private static AmmoCard ammoCard;
+    private static Match mockMatch;
+    private static Set<String> playersNames = new HashSet<>();
+    private static CoordPair location = new CoordPair(0,1);
+
+    @BeforeClass
+    public static void classSetUp() {
+        /* Load resources */
+        ModelTestUtil.loadCreatorResources();
+        playersNames.add("Sabbio");
+        playersNames.add("Andonio");
+        mockMatch = MatchCreator.createMatch(playersNames, new MatchConfiguration(0, 1));
+
+        Map<AmmoCube, Integer> map = new HashMap<>();
+        map.put(AmmoCube.BLUE, 3);
+        ammoCard = new AmmoCard(1, map, false);
+    }
 
     @Before
     public void setUp() throws Exception {
@@ -149,5 +172,20 @@ public class SpawnSquareTest {
     @Test
     public void isFull_newSquare_shouldReturnFalse() {
         assertFalse(square.isFull());
+    }
+
+    @Test
+    public void generateView_ShouldSucceed() {
+        Square square = mockMatch.getBoard().getSquare(location);
+        square.addPlayer(mockMatch.getPlayerByName("Sabbio"));
+        SpawnSquareView view = (SpawnSquareView) square.generateView();
+        assertEquals(square.getLocation(), view.getLocation());
+        assertEquals(square.getRoom().getColor(), view.getRoomColor());
+        assertEquals(square.getLocation(), view.getLocation());
+        assertTrue(view.getPlayers().containsAll(
+                square.getPlayers()
+                        .stream()
+                        .map(Player::getName)
+                        .collect(Collectors.toSet())));
     }
 }
