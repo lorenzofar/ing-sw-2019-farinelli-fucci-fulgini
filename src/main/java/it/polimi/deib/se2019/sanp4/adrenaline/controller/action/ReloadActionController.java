709b6ec8d08a500694967ecadd1c92c7deb6a8f5
Alessandro Fulgini <fuljo97@gmail.com>
2019-06-07 16:39:34 +0200

    Add ReloadActionController

--- src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/controller/action/ReloadActionController.java
+++ src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/controller/action/ReloadActionController.java
@@ -1,5 +1,119 @@
 package it.polimi.deib.se2019.sanp4.adrenaline.controller.action;
 
+import it.polimi.deib.se2019.sanp4.adrenaline.common.requests.WeaponCardRequest;
+import it.polimi.deib.se2019.sanp4.adrenaline.controller.ControllerFactory;
+import it.polimi.deib.se2019.sanp4.adrenaline.controller.PaymentHandler;
+import it.polimi.deib.se2019.sanp4.adrenaline.controller.PersistentView;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.items.ammo.AmmoCubeCost;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.items.weapons.UnloadedState;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.items.weapons.WeaponCard;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.match.Match;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.player.Player;
+import it.polimi.deib.se2019.sanp4.adrenaline.view.MessageType;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CancellationException;
+import java.util.stream.Collectors;
+
+/**
+ * Responsible for handling the basic action "Reload".
+ * The instance does not depend on the specific player who is performing the action.
+ * The actions gives the ability to the user to reload as many weapons as he wants
+ */
 public class ReloadActionController {
-    /* TODO: Implement this class */
+
+    private static final String MESSAGE_SELECT_WEAPON_TO_RELOAD = "Select the weapon you want to reload";
+
+    private static final String MESSAGE_CANT_PAY_RELOAD_COST = "You don't have enough items to reload the weapon";
+
+    private final Match match;
+
+    private final PaymentHandler paymentHandler;
+
+    /**
+     * Creates a new controller for the basic action "Reload".
+     * This instance can be used match-wide to control multiple iterations of the "Reload" action.
+     * @param match The match that has to be controlled, not null
+     * @param controllerFactory The controller factory to create needed controllers, not null
+     */
+    public ReloadActionController(Match match, ControllerFactory controllerFactory) {
+        this.match = match;
+        this.paymentHandler = controllerFactory.createPaymentHandler();
+    }
+
+    /**
+     * Executes the basic action "Reload" on the player associated to given view.
+     * The user is asked to select a weapon among the unloaded ones he has.
+     * Then he's asked to pay the cost for that weapon.
+     * If he doesn't have enough items to reload the selected weapon, he'll get a notification message.
+     * In either case (the selected weapon has been reloaded or not), the user will be asked to select
+     * another weapon to reload, until he selects no weapon or he has no more unloaded weapons.
+     * @param view The view associated to the player who performs the action, not null
+*      @throws CancellationException If the request to the player gets cancelled
+     * @throws InterruptedException If the thread is interrupted while performing the request
+     */
+    public void execute(PersistentView view) throws InterruptedException {
+        Player player = match.getPlayerByName(view.getUsername());
+
+        /* Determine the weapons which can be reloaded */
+        List<WeaponCard> selectable = player.getWeapons().stream()
+                .filter(w -> !w.getState().isUsable())
+                .collect(Collectors.toList());
+
+        while (!selectable.isEmpty()) {
+
+            WeaponCard selectedWeapon = askToSelectWeapon(view, selectable);
+
+            if (selectedWeapon != null) {
+                /* Ask the player to pay the cost and reload the weapon */
+                askToReloadWeapon(view, selectedWeapon);
+
+                /* Remove the weapon from the selection */
+                selectable.remove(selectedWeapon);
+            } else {
+                break; /* The user chose not to reload */
+            }
+        }
+    }
+
+    /**
+     * Asks the user to select one or none of the provided weapons to be reloaded
+     * @param view The view of the player whom to send the request, noy null
+     * @param choices The weapons from which to choose, not null
+     * @return The selected weapon or {@code null} if the player made no selection
+     * @throws CancellationException If the request to the player gets cancelled
+     * @throws InterruptedException If the thread is interrupted while performing the request
+     */
+    private WeaponCard askToSelectWeapon(PersistentView view, List<WeaponCard> choices) throws InterruptedException {
+        WeaponCardRequest req = new WeaponCardRequest(MESSAGE_SELECT_WEAPON_TO_RELOAD, choices, true);
+        return view.sendChoiceRequest(req).get();
+    }
+
+    /**
+     * Makes the user pay the reload cost of the given weapon.
+     * If the player can pay the cost, the weapon is actually reloaded,
+     * if he can't he will get a notification message.
+     * @param view The view of the player who wants to reload the weapon
+     * @param weapon The weapon that has to be reloaded, not null and in {@link UnloadedState}
+     * @throws CancellationException If a request to the player gets cancelled
+     * @throws InterruptedException If the thread is interrupted while performing the request
+     */
+    private void askToReloadWeapon(PersistentView view, WeaponCard weapon) throws InterruptedException {
+        Player player = match.getPlayerByName(view.getUsername());
+
+        /* Get the reload cost of the weapon and convert it to a map */
+        Map<AmmoCubeCost, Integer> costMap = AmmoCubeCost.mapFromCollection(weapon.getCost());
+
+        /* Ask the player to provide the ammo to reload the weapon */
+        boolean canPay = paymentHandler.payAmmoCost(view, costMap, false);
+
+        if (canPay) {
+            /* Then reload the weapon */
+            weapon.getState().reload(player, weapon);
+        } else {
+            /* Notify the player that he can't pay the cost */
+            view.showMessage(MESSAGE_CANT_PAY_RELOAD_COST, MessageType.WARNING);
+        }
+    }
 }
---
+++ src/test/java/it/polimi/deib/se2019/sanp4/adrenaline/controller/action/ReloadActionControllerTest.java
@@ -0,0 +1,174 @@
+package it.polimi.deib.se2019.sanp4.adrenaline.controller.action;
+
+import it.polimi.deib.se2019.sanp4.adrenaline.common.exceptions.NotEnoughAmmoException;
+import it.polimi.deib.se2019.sanp4.adrenaline.common.requests.WeaponCardRequest;
+import it.polimi.deib.se2019.sanp4.adrenaline.controller.ControllerFactory;
+import it.polimi.deib.se2019.sanp4.adrenaline.controller.PaymentHandler;
+import it.polimi.deib.se2019.sanp4.adrenaline.controller.PersistentView;
+import it.polimi.deib.se2019.sanp4.adrenaline.controller.answerers.FirstChoiceAnswer;
+import it.polimi.deib.se2019.sanp4.adrenaline.controller.answerers.SendChoiceRequestAnswer;
+import it.polimi.deib.se2019.sanp4.adrenaline.controller.requests.CompletableChoice;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.ModelTestUtil;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.items.ammo.AmmoCube;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.items.weapons.LoadedState;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.items.weapons.UnloadedState;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.items.weapons.WeaponCard;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.items.weapons.WeaponCreator;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.match.Match;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.match.MatchConfiguration;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.match.MatchCreator;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.player.Player;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ReloadActionControllerTest {
+
+    private static Match match;
+    private static String name = "bzoto";
+    private static Player player;
+
+    @Mock
+    private static PersistentView view;
+    @Mock
+    private static ControllerFactory factory;
+
+    /* NOTE TO MAINTAINER: WE USE A SINGLE INSTANCE OF MATCH, SO CLEAN THE SIDE EFFECTS AFTER THE TESTS */
+    @BeforeClass
+    public static void classSetup() throws NotEnoughAmmoException {
+        /* Disable logging */
+        ModelTestUtil.disableLogging();
+
+        /* Load schemas and assets */
+        ModelTestUtil.loadCreatorResources();
+
+        MatchConfiguration validConfig = new MatchConfiguration(0, 5);
+
+        /* Create a match */
+        match = MatchCreator.createMatch(Collections.singleton(name), validConfig);
+
+        /* Save some static variables */
+        player = match.getPlayerByName(name);
+
+        /* Remove initial ammo from the player */
+        player.payAmmo(player.getAmmo());
+    }
+
+    @Before
+    public void setUp() {
+        /* The view of the player must respond with his name */
+        when(view.getUsername()).thenReturn(name);
+        /* Return the payment handler when needed */
+        when(factory.createPaymentHandler()).thenReturn(new PaymentHandler(match));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        /* Revert the changes */
+        player.payAmmo(player.getAmmo()); /* Put back the ammo */
+
+        /* Remove any weapon from the player */
+        for (WeaponCard w : player.getWeapons()) {
+            player.removeWeapon(w);
+        }
+    }
+
+    @Test
+    public void execute_noWeaponsToReload_shouldNotPromptUser() throws Exception {
+        /* Add some loaded weapons to the player */
+        String[] wIds = {"shotgun", "thor"};
+        for (String id : wIds) {
+            WeaponCard w = WeaponCreator.createWeaponCard(id);
+            w.setState(new LoadedState());
+            player.addWeapon(w);
+        }
+
+        /* Execute the reload action */
+        ReloadActionController controller = new ReloadActionController(match, factory);
+        controller.execute(view);
+
+        /* Check no interaction with the user */
+        verify(view, never()).sendChoiceRequest(any());
+        verify(view, never()).showMessage(anyString(), any());
+    }
+
+    @Test
+    public void execute_weaponToReload_playerCanPay_shouldBeReloaded() throws Exception {
+        /* Add an unloaded weapon to the player */
+        String[] wIds = {"thor"};
+        for (String id : wIds) {
+            WeaponCard w = WeaponCreator.createWeaponCard(id);
+            w.setState(new UnloadedState());
+            player.addWeapon(w);
+        }
+
+        /* Give ammo to pay the reload cost */
+        player.addAmmo(AmmoCube.RED);
+        player.addAmmo(AmmoCube.BLUE);
+
+        /* Set up the user's answer: choose the first and only card */
+        when(view.sendChoiceRequest(any(WeaponCardRequest.class))).thenAnswer(new FirstChoiceAnswer());
+
+        /* Execute the reload action */
+        ReloadActionController controller = new ReloadActionController(match, factory);
+        controller.execute(view);
+
+        /* Check that the weapon has been reloaded */
+        Optional<WeaponCard> w = player.getWeapons().stream()
+                .filter(c -> c.getId().equals("thor")).findFirst();
+        assertTrue(w.isPresent());
+        assertTrue(w.get().getState().isUsable());
+
+        /* Check that the player paid the cost */
+        Map<AmmoCube, Integer> playerAmmo = player.getAmmo();
+        playerAmmo.forEach((cube, count) -> assertEquals(0, (int) count));
+    }
+
+    @Test
+    public void execute_weaponsToReload_playerCantPayFirst_shouldBeAskedToSelectSecond() throws Exception {
+        /* Add two unloaded weapons to the player */
+        WeaponCard thor = WeaponCreator.createWeaponCard("thor"); /* Cost: RB */
+        WeaponCard tb = WeaponCreator.createWeaponCard("tractor_beam"); /* Cost: B */
+        thor.setState(new UnloadedState());
+        tb.setState(new UnloadedState());
+        player.addWeapon(thor);
+        player.addWeapon(tb);
+
+        /* Give ammo to pay the reload cost of TB */
+        player.addAmmo(AmmoCube.BLUE);
+
+        /* Set up the user's answer: choose the first and only card */
+        when(view.sendChoiceRequest(any(WeaponCardRequest.class)))
+                /* First answer: choose thor -> cannot reload */
+                .thenAnswer((SendChoiceRequestAnswer<WeaponCard>) req ->
+                        new CompletableChoice<>(req).complete(thor)
+                ).thenAnswer((SendChoiceRequestAnswer<WeaponCard>) req ->
+                        new CompletableChoice<>(req).complete(tb)
+                );
+
+        /* Execute the reload action */
+        ReloadActionController controller = new ReloadActionController(match, factory);
+        controller.execute(view);
+
+        /* Check that the weapon has been reloaded */
+        Optional<WeaponCard> w = player.getWeapons().stream()
+                .filter(c -> c.getId().equals("tractor_beam")).findFirst();
+        assertTrue(w.isPresent());
+        assertTrue(w.get().getState().isUsable());
+
+        /* Check that the player paid the cost */
+        Map<AmmoCube, Integer> playerAmmo = player.getAmmo();
+        playerAmmo.forEach((cube, count) -> assertEquals(0, (int) count));
+    }
+}
