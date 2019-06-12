40f55148c192eb7288ca1a0f207fa2ca29c64ef9
Alessandro Fulgini <fuljo97@gmail.com>
2019-06-12 18:30:14 +0200

    Add AbstractTarget and SingleSquareTarget

---
+++ src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/controller/weapons/targets/AbstractTarget.java
@@ -0,0 +1,244 @@
+package it.polimi.deib.se2019.sanp4.adrenaline.controller.weapons.targets;
+
+import it.polimi.deib.se2019.sanp4.adrenaline.controller.ControllerFactory;
+import it.polimi.deib.se2019.sanp4.adrenaline.controller.PersistentView;
+import it.polimi.deib.se2019.sanp4.adrenaline.controller.weapons.AbstractWeapon;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.board.VisibilityEnum;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.match.Match;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.player.Player;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.CancellationException;
+
+import static it.polimi.deib.se2019.sanp4.adrenaline.model.board.VisibilityEnum.VISIBLE;
+
+public abstract class AbstractTarget {
+
+    protected final AbstractWeapon weapon;
+
+    protected final Match match;
+
+    protected final ControllerFactory factory;
+
+    protected PersistentView view;
+
+    protected final String id;
+
+    protected boolean optional;
+
+    protected int damage;
+
+    protected int marks;
+
+    protected VisibilityEnum visibility;
+
+    protected Integer minDist;
+
+    protected Integer maxDist;
+
+    protected Set<String> excludePlayers;
+
+    protected Set<String> excludeSquares;
+
+    /**
+     * Creates a new abstract target associated to the given weapon.
+     * While the id is set on initialization, the other properties receive their default values,
+     * which can be changed later by using setters.
+     *
+     * @param id      The unique identifier of this target in its effect, not null
+     * @param weapon  The weapon controller associated to this target, not null
+     * @param match   The match which has to be controlled, not null
+     * @param factory The factory to create needed controllers, not null
+     */
+    public AbstractTarget(String id, AbstractWeapon weapon, Match match, ControllerFactory factory) {
+        this.id = id;
+        this.weapon = weapon;
+        this.match = match;
+        this.factory = factory;
+
+        /* Set defaults */
+        this.optional = false; /* Mandatory */
+        this.damage = this.marks = 0; /* No damages nor marks */
+        this.visibility = VISIBLE; /* Only visible squares */
+        this.minDist = this.maxDist = null; /* Minimum and maximum distance not checked */
+        this.excludePlayers = Collections.emptySet(); /* Do not exclude players */
+        this.excludeSquares = Collections.emptySet(); /* Do noy exclude squares */
+    }
+
+    /* ====================== GETTERS AND SETTERS ========================= */
+
+    /**
+     * Returns the identifier of this target
+     *
+     * @return The identifier of this target
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Returns whether this target is optional or not
+     *
+     * @return {@code true} if this target is optional, {@code false} otherwise
+     */
+    public boolean isOptional() {
+        return optional;
+    }
+
+    /**
+     * Sets this weapon's optional attribute.
+     * If set to {@code true}, then the user won't be obliged to shoot this
+     * target while using the weapon.
+     * Default value is {@code false}.
+     *
+     * @param optional The desired value of the attribute
+     */
+    public void setOptional(boolean optional) {
+        this.optional = optional;
+    }
+
+    /**
+     * Returns the number of damage tokens which will be applied to the targeted players.
+     * @return The number of damage tokens which will be applied to the targeted players.
+     */
+    public int getDamage() {
+        return damage;
+    }
+
+    /**
+     * Sets the number of damage tokens which will be applied to the targeted players.
+     * @param damage The number of damage tokens, non-negative
+     */
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    /**
+     * Returns the number of marks which will be applied to the targeted players.
+     * @return The number of marks which will be applied to the targeted players.
+     */
+    public int getMarks() {
+        return marks;
+    }
+
+    /**
+     * Sets the number of damage tokens which will be applied to the targeted players.
+     * @param marks The number of damage tokens which will be applied to the targeted players.
+     */
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    /**
+     * Returns the visibility constraint applied for square selection
+     * @return The visibility constraint applied for square selection
+     */
+    public VisibilityEnum getVisibility() {
+        return visibility;
+    }
+
+    /**
+     * Sets the visibility constraint applied for square selection
+     * @param visibility The visibility constraint applied for square selection, not null
+     */
+    public void setVisibility(VisibilityEnum visibility) {
+        this.visibility = visibility;
+    }
+
+    /**
+     * Returns the minimum distance constraint for square selection
+     * @return The minimum distance, null if not set
+     */
+    public Integer getMinDist() {
+        return minDist;
+    }
+
+    /**
+     * Sets the minimum distance constraint for square selection
+     * @param minDist The minimum distance for square selection.
+     *                A null value means no constraint, an integer value must be positive.
+     */
+    public void setMinDist(Integer minDist) {
+        this.minDist = minDist;
+    }
+
+    /**
+     * Returns the maximum distance constraint for square selection
+     * @return The maximum distance, null if not set
+     */
+    public Integer getMaxDist() {
+        return maxDist;
+    }
+
+    /**
+     * Sets the maximum distance constraint for square selection
+     * @param maxDist The maximum distance for square selection.
+     *                A null value means no constraint, an integer value must be positive.
+     */
+    public void setMaxDist(Integer maxDist) {
+        this.maxDist = maxDist;
+    }
+
+    /**
+     * Returns the set of players which have to be excluded from selection for this target.
+     * The players are represented by the ids of their targets in the current weapon.
+     * @return A set of ids representing the players to exclude
+     */
+    public Set<String> getExcludePlayers() {
+        return excludePlayers;
+    }
+
+    /**
+     * Sets the set of players which have to be excluded from selection for this target.
+     * The players are represented by the ids of their targets in the current weapon.
+     * @param excludePlayers The set of ids representing the players to exclude, not null
+     */
+    public void setExcludePlayers(Set<String> excludePlayers) {
+        this.excludePlayers = excludePlayers;
+    }
+
+    /**
+     * Returns the set of squares which have to be excluded from selection for this target.
+     * The squares are represented by the ids of their targets in the current weapon.
+     * @return A set of ids representing the squares to exclude
+     */
+    public Set<String> getExcludeSquares() {
+        return excludeSquares;
+    }
+
+    /**
+     * Sets the set of squares which have to be excluded from selection for this target.
+     * The squares are represented by the ids of their targets in the current weapon.
+     * @param excludeSquares The set of ids representing the squares to exclude, not null
+     */
+    public void setExcludeSquares(Set<String> excludeSquares) {
+        this.excludeSquares = excludeSquares;
+    }
+
+    /* ===================== UTILITIES ======================== */
+
+    /**
+     * Returns a set of players who can't be targeted for the following reasons:
+     * explicitly excluded, or because he's the shooter.
+     * @param shooter The player using the weapon, not null
+     * @return The set of players who can't be targeted
+     */
+    protected Set<Player> determineUntargetablePlayers(Player shooter) {
+        Set<Player> set = weapon.getSavedPlayers(excludePlayers);
+        set.add(shooter);
+        return set;
+    }
+
+    /* ====================== EXECUTE ========================= */
+
+    /**
+     * Executes this target given the user of the weapon
+     * @param view The view of the player using the weapon
+     * @return {@code true} if the player chose his target and was able to perform damage/marks
+     * {@code false} if the player chose not to use the target
+     * @throws CancellationException if a request to the user gets cancelled
+     * @throws InterruptedException if the thread gets interrupted
+     */
+    public abstract boolean execute(PersistentView view) throws InterruptedException;
+}
---
+++ src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/controller/weapons/targets/SingleSquareTarget.java
@@ -0,0 +1,200 @@
+package it.polimi.deib.se2019.sanp4.adrenaline.controller.weapons.targets;
+
+import it.polimi.deib.se2019.sanp4.adrenaline.controller.ControllerFactory;
+import it.polimi.deib.se2019.sanp4.adrenaline.controller.PersistentView;
+import it.polimi.deib.se2019.sanp4.adrenaline.controller.weapons.AbstractWeapon;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.board.Board;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.board.CardinalDirection;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.board.Square;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.match.Match;
+import it.polimi.deib.se2019.sanp4.adrenaline.model.player.Player;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.CancellationException;
+
+import static it.polimi.deib.se2019.sanp4.adrenaline.controller.weapons.ShootingDirectionEnum.CARDINAL;
+
+public abstract class SingleSquareTarget extends AbstractTarget {
+
+    protected Set<String> chooseBetweenTargets;
+
+    protected String visibleFromPlayer;
+
+    protected boolean moveShooterHere;
+
+    protected String squareRef;
+
+    /**
+     * Creates a new single square target associated to the given weapon.
+     * While the id is set on initialization, the other properties receive their default values,
+     * which can be changed later by using setters.
+     *
+     * @param id      The unique identifier of this target in its effect, not null
+     * @param weapon  The weapon controller associated to this target, not null
+     * @param match   The match which has to be controlled, not null
+     * @param factory The factory to create needed controllers, not null
+     */
+    public SingleSquareTarget(String id, AbstractWeapon weapon, Match match, ControllerFactory factory) {
+        super(id, weapon, match, factory);
+
+        /* Set default values */
+        chooseBetweenTargets = null; /* Choose between any targets you want */
+        visibleFromPlayer = null; /* No constraint */
+        moveShooterHere = false; /* Don't move shooter to target square */
+        squareRef = null; /* Do not save a reference to target's square */
+    }
+
+    /* ====================== GETTERS AND SETTERS ========================= */
+
+    /**
+     * Returns the set of target ids from which the shooter can choose his target.
+     *
+     * @return The set of target ids from which the shooter can choose his target,
+     * null if there is no such constraint
+     */
+    public Set<String> getChooseBetweenTargets() {
+        return chooseBetweenTargets;
+    }
+
+    /**
+     * Sets the set of target ids from which the shooter can choose his target.
+     *
+     * @param chooseBetweenTargets Set of target ids from which the shooter can choose his target,
+     *                             null if no such constraint
+     */
+    public void setChooseBetweenTargets(Set<String> chooseBetweenTargets) {
+        this.chooseBetweenTargets = chooseBetweenTargets;
+    }
+
+    /**
+     * Returns the id of the player target from which this target needs to be visible.
+     *
+     * @return The id of the player target from which this target needs to be visible,
+     * null if there is no such constraint
+     */
+    public String getVisibleFromPlayer() {
+        return visibleFromPlayer;
+    }
+
+    /**
+     * Sets the id of the player target from which this target needs to be visible.
+     *
+     * @param visibleFromPlayer The id of the player target from which this target needs to be visible,
+     *                          null if no such constraint
+     */
+    public void setVisibleFromPlayer(String visibleFromPlayer) {
+        this.visibleFromPlayer = visibleFromPlayer;
+    }
+
+    /**
+     * Returns whether the shooter will be moved to the target's square after shooting or not.
+     *
+     * @return {@code true} if the shooter will be moved to the target's square after shooting, {@code false} otherwise
+     */
+    public boolean isMoveShooterHere() {
+        return moveShooterHere;
+    }
+
+    /**
+     * Sets whether the shooter has to be moved to the target's square after shooting or not.
+     *
+     * @param moveShooterHere whether the shooter has to be moved to the target's square after shooting or not
+     */
+    public void setMoveShooterHere(boolean moveShooterHere) {
+        this.moveShooterHere = moveShooterHere;
+    }
+
+    /**
+     * Returns the id with which the target's square will be saved in the weapon.
+     *
+     * @return The id with which the target's square will be saved in the weapon, null if it won't be saved.
+     */
+    public String getSquareRef() {
+        return squareRef;
+    }
+
+    /**
+     * Sets the id with which the target's square has to be saved in the weapon.
+     *
+     * @param squareRef The id with which the target's square has to be saved in the weapon,
+     *                  null if it doesn't have to be saved.
+     */
+    public void setSquareRef(String squareRef) {
+        this.squareRef = squareRef;
+    }
+
+    /* ====================== UTILITY ========================= */
+
+    /**
+     * Determines and returns the squares which are selectable for
+     * targeting according to the following constraints:
+     * <ul>
+     * <li>Direction of the weapon (if already set)</li>
+     * <li>Visibility</li>
+     * <li>Minimum and maximum distance</li>
+     * <li>Squares to be excluded via {@code excludeSquares}</li>
+     * <li>Visibility from specified player target ({@code visibleFromPlayer})</li>
+     * </ul>
+     *
+     * @param start The square from which to calculate distances, not null
+     * @return The selectable squares according to given criteria
+     */
+    protected Set<Square> determineSelectableSquares(Square start) {
+        /* Get a hook to the board */
+        Board board = match.getBoard();
+
+        Set<Square> query = new HashSet<>();
+
+        /* Handle the direction */
+        CardinalDirection direction = weapon.getSelectedDirection();
+        if (weapon.getShootingDirection() == CARDINAL && direction == null) {
+            /* Then we query squares in straight direction in all the directions */
+            for (CardinalDirection d : CardinalDirection.values()) {
+                query.addAll(board.querySquares(start, visibility, d, minDist, maxDist));
+            }
+        } else {
+            /* We query squares in all directions or in the selected direction */
+            query.addAll(board.querySquares(start, visibility, direction, minDist, maxDist));
+        }
+
+        /* Exclude specified squares */
+        Set<Square> toExclude = weapon.getSavedSquares(excludeSquares);
+
+        query.removeAll(toExclude);
+
+        /* Intersect with squares visible from specified player */
+        if (visibleFromPlayer != null) {
+            /* Get the player associated to that target */
+            Player him = weapon.getSavedPlayer(visibleFromPlayer);
+
+            /* Determine the squares he can see */
+            Set<Square> visibleFromHim;
+            if (him != null) {
+                /* Determine the squares he can see */
+                visibleFromHim = board.getVisibleSquares(him.getCurrentSquare());
+            } else {
+                visibleFromHim = new HashSet<>();
+            }
+
+            /* Intersect them with the ones we already have */
+            query.retainAll(visibleFromHim);
+        }
+
+        return query;
+    }
+
+    /* ====================== EXECUTE ========================= */
+
+    /**
+     * Executes this target given the user of the weapon
+     *
+     * @param view The view of the player using the weapon
+     * @return {@code true} if the player chose his target and was able to perform damage/marks
+     * {@code false} if the player chose not to use the target
+     * @throws CancellationException if a request to the user gets cancelled
+     * @throws InterruptedException  if the thread gets interrupted
+     */
+    @Override
+    public abstract boolean execute(PersistentView view) throws InterruptedException;
+}
