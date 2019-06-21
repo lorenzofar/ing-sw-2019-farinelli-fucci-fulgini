7a4d994ca171ebc6b302956a6bdf38908817ce02
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2019-06-21 10:42:39 +0200

    Fix AmmoPane control and improve its layout

--- src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/client/gui/controls/AmmoPane.java
+++ src/main/java/it/polimi/deib/se2019/sanp4/adrenaline/client/gui/controls/AmmoPane.java
@@ -4,10 +4,12 @@ import it.polimi.deib.se2019.sanp4.adrenaline.client.gui.GUIRenderer;
 import it.polimi.deib.se2019.sanp4.adrenaline.model.items.ammo.AmmoCube;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+import java.util.EnumMap;
 import java.util.Map;
 
 /**
@@ -20,26 +22,29 @@ public class AmmoPane extends VBox {
      */
     private Map<AmmoCube, Label> cubeLabels;
 
-    AmmoPane() {
+    public AmmoPane() {
         super();
         this.setSpacing(8);
         this.getStylesheets().add("/fxml/style.css");
 
-        Label header = new Label("Available ammo");
+        Label header = new Label("Ammo");
         header.getStyleClass().add(GUIRenderer.CSS_BOLD_TITLE);
-        this.getChildren().add(header);
-
+        cubeLabels = new EnumMap<>(AmmoCube.class);
+        HBox cubesContainer = new HBox(8);
+        this.getChildren().addAll(header, cubesContainer);
         // Then for each cube color, create a label and add to the corresponding map
         for (AmmoCube cube : AmmoCube.values()) {
             // Create a container
-            HBox cubeContainer = new HBox(4);
+            StackPane cubeContainer = new StackPane();
             // Add a square with the color of the cube
-            cubeContainer.getChildren().add(new Rectangle(4, 4, Color.web(cube.getHexCode())));
+            Rectangle ammoCube = new Rectangle(24, 24, Color.web(cube.getHexCode(), 0.7));
             // Then add a label to show the amount
             Label cubeLabel = new Label("0");
-            cubeContainer.getChildren().add(cubeLabel);
-            // And save it in the map
+            // Save in the map
             cubeLabels.put(cube, cubeLabel);
+            // And add to the corresponding parent
+            cubeContainer.getChildren().addAll(ammoCube, cubeLabel);
+            cubesContainer.getChildren().add(cubeContainer);
         }
     }
 
@@ -51,14 +56,14 @@ public class AmmoPane extends VBox {
     public void setAmmo(Map<AmmoCube, Integer> ammo) {
         // For each of the cubes contained in the map,
         // retrieve the corresponding label and update the amount
-        ammo.entrySet().forEach(e -> {
+        ammo.forEach((cube, amount) -> {
             // Retrieve the corresponding label
-            Label cubeLabel = cubeLabels.get(e.getKey());
+            Label cubeLabel = cubeLabels.get(cube);
             // And if is not null, update its value
             // A null retrieved label would never happen, however, since
             // both the client and the server share the same list of cube colors
             if (cubeLabel != null) {
-                cubeLabel.setText(e.getValue().toString());
+                cubeLabel.setText(amount.toString());
             }
         });
     }
